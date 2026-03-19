a0f71cb69bfe0126b9aec5d8821ccf44525ce1a5
Yaroslav Shevchenko <yarben03@gmail.com>
2026-03-19 17:20:51 -0400

    Created Rectangle class which inherit properties from basic shape. Created Square class that is goint to inherit directly from Rectangle

--- shapes.py
+++ shapes.py
@@ -66,3 +66,56 @@ class Circle(BasicShape):
         self._radius = value
 
         self.calc_area()
+
+class Rectangle(BasicShape):
+    def __init__(self, l, w, n="Rectangle"):
+        super().__init__(name=n)
+
+        self._length = l
+        self._width = w
+
+        self.calc_area()
+
+    def calc_area(self):
+        """Computes the rectangle's area (length * width) and stores it."""
+        self.area = self._length * self._width
+
+    @property
+    def length(self):
+        return self._length
+
+    @length.setter
+    def length(self, value):
+        self._length = value
+        self.calc_area()
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        self._width = value
+
+        self.calc_area()
+
+class Square(Rectangle):
+    def __init__(self, s, n="Square"):
+
+        super().__init__(s, s, n)
+
+        self._side = s
+
+        self.name = n
+
+    @property
+    def side(self):
+        return self._side
+
+    @side.setter
+    def side(self, value):
+        self._side = value
+
+        self.length = value
+        self.width = value
+
